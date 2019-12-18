9b77855d03f310cc00ad8d9c470da24a8577d359
Vigge <Viktor.persson91@gmail.com>
2019-12-18 17:58:16 +0100

    Added a navigationFrame to the button in XAML. To work with when we gonna change the view.

--- ProjectRecept/MainPage.xaml.cs
+++ ProjectRecept/MainPage.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using ProjectRecept.Views;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ProjectRecept
